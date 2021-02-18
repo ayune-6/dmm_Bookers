92c8bcd80571beb49e6e0603244eda5cd17ba979
ayune-6 <ayune19990823@gmail.com>
2021-02-18 03:01:04 +0000

    [20210118]AddedActions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,41 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy(book_params)
+    redirect_to '/'
+  end
+
+  private
+  def book_params
+    params.require(:Book).permit(:title, :body)
+  end
+
 end
